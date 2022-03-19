25d8d876adc5fbdf9bd430bd2bbceee359c051ba
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-03-19 15:45:05 -0300

    creating task management service

--- src/app/tasks/shared/task.model.ts
+++ src/app/tasks/shared/task.model.ts
@@ -1,4 +1,4 @@
-export class Tasks {
+export class Task {
 
   constructor(
     public id?: number,
---
+++ src/app/tasks/shared/task.service.ts
@@ -0,0 +1,54 @@
+import {Injectable} from '@angular/core';
+import {Task} from "./task.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor() {
+  }
+
+  listAll(): Task[] {
+    const tasks = localStorage['tasks'];
+    return tasks ? JSON.parse(tasks) : [];
+  }
+
+  register(task: Task): void {
+    const tasks = this.listAll();
+    task.id = new Date().getTime();
+    tasks.push(task);
+    localStorage['tasks'] = JSON.stringify(tasks);
+  }
+
+  findById(id: number): Task {
+    const tasks: Task[] = this.listAll();
+    return tasks.find(task => task.id === id);
+  }
+
+  update(task: Task): void {
+    const tasks: Task[] = this.listAll();
+    tasks.forEach((obj, index, objs) => {
+      if (task.id === obj.id) {
+        objs[index] = task;
+      }
+    });
+    localStorage['tasks'] = JSON.stringify(tasks);
+  }
+
+  remove(id: number): void {
+    let tasks: Task[] = this.listAll();
+    tasks = tasks.filter(task => task.id !== id);
+    localStorage['tasks'] = JSON.stringify(tasks);
+  }
+
+  changeStatus(id: number): void {
+    const tasks: Task[] = this.listAll();
+    tasks.forEach((obj, index, objs) => {
+      if (id === obj.id) {
+        objs[index].completed = !obj.completed;
+      }
+    });
+    localStorage['tasks'] = JSON.stringify(tasks);
+  }
+}
