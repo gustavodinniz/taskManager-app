12744ab70c3a872d573b9f474d30be4d6450dac7
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-03-19 18:42:30 -0300

    creating component register-task

--- src/app/tasks/list/list-task.component.ts
+++ src/app/tasks/list/list-task.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Task, TaskService} from '../shared'
 
 @Component({
   selector: 'app-list-task',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListTaskComponent implements OnInit {
 
-  constructor() { }
+  tasks: Task[];
+
+  constructor(private taskService: TaskService) {
+  }
 
   ngOnInit(): void {
+    this.tasks = this.listAll();
+    this.tasks = [
+      new Task(1, "Tarefa 1", false),
+      new Task(2, "Tarefa 2", true),
+      new Task(3, "Tarefa 3", false),
+      new Task(4, "Tarefa 4", true),
+      new Task(5, "Tarefa 5", false),
+    ]
+  }
+
+  listAll(): Task[] {
+    return this.taskService.listAll();
+  }
+
+  remove($event: any, task: Task): void {
+    $event.preventDefault();
+    if (confirm('Deseja remover a Tarefa "' + task.name + '"?')) {
+      this.taskService.remove(task.id);
+      this.tasks = this.listAll();
+    }
+  }
+
+  changeStatus(task: Task): void {
+    if (confirm('Deseja alterar o status da Tarefa "' + task.name + '"?')) {
+      this.taskService.changeStatus(task.id);
+      this.tasks = this.listAll();
+    }
   }
 
 }
--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,15 +1,21 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {RouterModule} from "@angular/router";
+import {FormsModule} from "@angular/forms";
 import {TaskService} from "./shared";
-import { ListTaskComponent } from './list/list-task.component';
+import {ListTaskComponent} from './list/list-task.component';
+import { RegisterTaskComponent } from './register/register-task.component';
 
 
 @NgModule({
   declarations: [
-    ListTaskComponent
+    ListTaskComponent,
+    RegisterTaskComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [
     TaskService
